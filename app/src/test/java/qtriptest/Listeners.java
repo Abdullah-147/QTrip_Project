d11a348520c6bd1e3581264c0514b34b6db9afe9
Abdul Jamadar <abdullahjamadar01@gmail.com>
2024-03-11 09:42:14 +0530

    adding log4j implementation

--- app/src/test/java/qtriptest/Listeners.java
+++ app/src/test/java/qtriptest/Listeners.java
@@ -1,12 +1,11 @@
 package qtriptest;
 
-import qtriptest.tests.BaseTest;
 import java.io.File;
 import java.io.IOException;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -14,13 +13,21 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import qtriptest.tests.BaseTest;
+
 public class Listeners implements ITestListener{
 
     static ExtentReports reports;
+    static Logger logger=LogManager.getLogger(Listeners.class);
     ExtentTest test;
     @Override
     public void onStart(ITestContext c){
         reports=ReportSingleton.createExtentReport();
+        logger.info("Test Begins at "+java.time.LocalDateTime.now());
     }
 
     @Override
@@ -35,6 +42,7 @@ public class Listeners implements ITestListener{
         } catch (IOException e) {
             e.printStackTrace();
         }
+        logger.error(result.getName()+"- This test case failed!");
         reports.endTest(test);
     }
 
@@ -46,6 +54,7 @@ public class Listeners implements ITestListener{
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        logger.info(result.getName()+"- This test case passed!");
         reports.endTest(test);
     }
 
@@ -53,11 +62,13 @@ public class Listeners implements ITestListener{
     public void onTestSkipped(ITestResult result){
         test.log(LogStatus.SKIP,result.getName()+" skipped.");
         reports.endTest(test);
+        logger.warn(result.getName()+"- This test case was skipped.");
     }
 
     @Override
     public void onFinish(ITestContext c){
         reports.flush();
+        logger.info("Test Ends at "+java.time.LocalDateTime.now());
     }
 
     public static String screenCapture(WebDriver driver,String testName,String status) throws IOException{
